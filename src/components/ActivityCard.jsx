ec199fc13e14df94cb5491371f5a69045caf7702
Nurmira16 <nurmirajorobek@gmail.com>
2025-07-28 22:55:41 +0200

    Integrate ActivityCard in ActivitySummary with state update and styling

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,15 +1,38 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { setWorkoutHours } from '../features/activities/activitySlice'
+import { setWorkoutHours,setCalories,setSteps } from '../features/activities/activitySlice'
+import '../styles/activity_card.scss'
 
-const ActivityCard = () => {
-  const { workoutHours } = useSelector(state => state.activities)
+const ActivityCard = ({type}) => {
   const dispatch = useDispatch()
+  const activities=useSelector(state=>state.activities)
+
+  const activityMap={
+    workout:{
+      label:'Workout',
+      value: activities.workoutHours,
+      increment: ()=>dispatch(setWorkoutHours(activities.workoutHours+1))
+    },
+    calories:{
+      label:'Calories',
+      value: activities.calories,
+      increment: ()=>dispatch(setCalories(activities.calories+100))
+    },
+    steps:{
+      label:'Steps',
+      value: activities.step,
+      increment: ()=>dispatch(setSteps(activities.steps+500))
+    }
+  }
+  if (!activityMap[type]) return null;
+  const {label,value,increment}=activityMap[type]
 
   return (
-    <div>
-      <h2>Workout: {workoutHours} hrs</h2>
-      <button onClick={() => dispatch(setWorkoutHours(workoutHours + 1))}>Add Hour</button>
-    </div>
+    <div className="activity-card">
+    <h2 className="activity-card__title">{label}: {value}</h2>
+    <button className="activity-card__button" onClick={increment}>
+      Add {label}
+    </button>
+  </div>
   )
 }
 
--- src/components/ActivitySummary.jsx
+++ src/components/ActivitySummary.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import '../styles/activity_summary.scss'
+import ActivityCard from './ActivityCard'
 
 
 const ActivitySummary = () => {
   const { workoutHours, calories, steps } = useSelector(state => state.activities)
+  const [selected,setSelected]=useState(null)
+
+  const handleClick=(type)=>{
+    setSelected(type)
+  }
 
   return (
     <div className="summary">
-      <div className="block workout">Workout: {workoutHours} hrs</div>
-      <div className="block calories">Calories: {calories} kcal</div>
-      <div className="block steps">Steps: {steps}</div>
+      <div className="block workout" onClick={()=>handleClick('workout')}>Workout: {workoutHours} hrs</div>
+      <div className="block calories" onClick={()=>handleClick('calories')}>Calories: {calories} kcal</div>
+      <div className="block steps" onClick={()=>handleClick('steps')}>Steps: {steps}</div>
+
+      {selected && <ActivityCard type={selected}/>}
+
     </div>
   )
 }
